665ce92c5bf38c22fb247dcd49004ac272262ae0
Nandita Gandhi <nandita.appknit@gmail.com>
2020-02-07 12:50:49 +0530

    Third commit

--- app/src/main/java/com/example/helloworld/Image.kt
+++ app/src/main/java/com/example/helloworld/Image.kt
@@ -10,19 +10,35 @@ class Image : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
+
+        lvList.setOnItemClickListener{_, _, index, _ ->
+            if(index==0) {
+                ivImage.setImageResource(R.drawable.mickey)}
+            else if(index==1){
+                ivImage.setImageResource(R.drawable.sea)}
+            else if(index==2){
+                ivImage.setImageResource(R.drawable.rose)}
+
+            else {
+                ivImage.setImageResource(R.drawable.kotlin)
+            }
+
+        }
+
+
     }
     fun radiobuttonclick(view:View){
-        if (view==rb_1){
-            ivImage.setImageResource(R.drawable.mickey)
-        }
+        if(view==rb_1) {
+             ivImage.setImageResource(R.drawable.mickey)}
         else if(view==rb_2){
-            ivImage.setImageResource(R.drawable.sea)
-        }
+            ivImage.setImageResource(R.drawable.sea)}
         else if(view==rb_3){
-            ivImage.setImageResource(R.drawable.rose)
-        }
+            ivImage.setImageResource(R.drawable.rose)}
+
         else {
-            ivImage.setImageResource(R.drawable.kotlin)
+                ivImage.setImageResource(R.drawable.kotlin)
+            }
         }
+
     }
-}
+
